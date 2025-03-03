09f2267c9e42fefe9e6e9f906ad2d0b899ecfbd3
Iryna <mefodievna2908@gmail.com>
2025-03-03 10:23:06 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,25 +4,23 @@ import css from './Header.module.css';
 const Header = () => {
   return (
     <header className={css.header}>
-    <div className={css.linksContainer}>
-
-
-  <Link className={css.logoLink} to = "/">
-  <img src="/icons/Logo.svg" alt="logo"  />
-  </Link>
-  <nav className={css.navLink}>
-    <ul className={css.carLink}>
-        <li>
-          <Link  to="/">Home</Link>
-          </li>
-          <li>
-          <Link  to="/catalog">Catalog</Link>
-          </li>
+      <div className={css.linksContainer}>
+        <Link className={css.logoLink} to="/">
+          <img src="/icons/Logo.svg" alt="logo" />
+        </Link>
+        <nav className={css.navLink}>
+          <ul className={css.carLink}>
+            <li className={css.itemLink}>
+              <Link to="/">Home</Link>
+            </li>
+            <li className={css.itemLink}>
+              <Link to="/catalog">Catalog</Link>
+            </li>
           </ul>  
         </nav>
-    </div>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;

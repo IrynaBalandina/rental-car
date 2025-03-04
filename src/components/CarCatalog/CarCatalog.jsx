3680e6a684df7d0f48a16ff9f0edd88ed12ebec9
Iryna <mefodievna2908@gmail.com>
2025-03-04 16:00:55 +0200

    correcting mistakes

--- src/components/CarCatalog/CarCatalog.jsx
+++ src/components/CarCatalog/CarCatalog.jsx
@@ -14,36 +14,49 @@ const CarCatalog = () => {
 
   const [filters, setFilters] = useState({ brand: "", rentalPrice: "", minMileage: "", maxMileage: "" });
   const [page, setPage] = useState(1);
+  const [allCars, setAllCars] = useState([]); 
   const [hasMore, setHasMore] = useState(true); 
 
   useEffect(() => {
-    dispatch(fetchCars({ limit: 12, page })); 
+    const fetchData = async () => {
+      const data = await dispatch(fetchCars({ limit: 12, page })).unwrap();
+      
+      if (data.length > 0) {
+        setAllCars(prevCars => [...prevCars, ...data]); 
+      } else {
+        setHasMore(false); 
+      }
+    };
+
+    fetchData();
   }, [dispatch, page]);
 
   const handleSearch = () => {
     setPage(1);
-    dispatch(fetchCars({ limit: 12, page: 1, ...filters }));
+    setAllCars([]); 
+    dispatch(fetchCars({ limit: 12, page: 1, ...filters })).then(({ payload }) => {
+      setAllCars(payload); 
+    });
   };
 
   const handleLoadMore = () => {
     setPage(prevPage => prevPage + 1);
   };
 
-  const brands = [...new Set(cars.map(car => car.brand))];
-  const prices = [...new Set(cars.map(car => car.rentalPrice))].sort((a, b) => a - b);
+  const brands = [...new Set(allCars.map(car => car.brand))];
+  const prices = [...new Set(allCars.map(car => car.rentalPrice))].sort((a, b) => a - b);
 
   return (
     <div>
       <FilterBox filters={filters} setFilters={setFilters} brands={brands} prices={prices} onSearch={handleSearch} /> 
 
       <div className={css.carCatalog}>
-        {isLoading && <p>Loading...</p>}
+        {isLoading && page === 1 && <p>Loading...</p>}
         {error && <p>❌ {error}</p>}
-        {cars.length === 0 && !isLoading && <p>No cars found</p>}
-        {cars.map((car) => <CarCard key={car.id} car={car} />)}
+        {allCars.length === 0 && !isLoading && <p>No cars found</p>}
+        {allCars.map((car) => <CarCard key={car.id} car={car} />)}
       </div>
 
- 
       {hasMore && !isLoading && (
         <div className={css.loadMoreContainer}>
           <button className={css.loadMoreBtn} onClick={handleLoadMore}>
